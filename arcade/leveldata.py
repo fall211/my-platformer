524a427f5f6b56e81081df9ce476998a860ec80c
fall211 <tuukka02@gmail.com>
2022-05-04 16:51:44 -0700

    locations for enemy spawns

--- arcade/leveldata.py
+++ arcade/leveldata.py
@@ -3,9 +3,11 @@
 
 
 level_1 = {
-    'player_spawn_pos': (830,960),
+    'player_spawn_pos': (830, 960),
+    'enemy_spawner_pos': [(2250, 630), (1000, 350)],
 }
 
 level_2 = {
-    'player_spawn_pos': (420,970)
+    'player_spawn_pos': (420, 970),
+    'enemy_spawner_pos': [(420, 420)],
 }
