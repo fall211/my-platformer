36d912f54f0ad1a372acb8c27601bb8340d539d1
fall211 <tuukka02@gmail.com>
2022-05-10 17:10:24 -0700

    death check fn

--- arcade/player.py
+++ arcade/player.py
@@ -10,7 +10,18 @@ class Player(Entity):
 
         self.x = 0
         self.y = 0
-        self.health = 100
+        self.is_alive = True
+        self.health = PLAYER_HEALTH
+
+    def on_update(self, delta_time: float = 1 / 60):
+
+        # Check if the player is dead
+        if self.health > 0:
+            self.is_alive = True
+        else:
+            self.is_alive = False
+
+        return super().on_update(delta_time)
 
     def update_animation(self, delta_time: float = 1/60):
         
